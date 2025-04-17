88b5d60b0e3bbc9636b72a39221e76024c6c235c
praveenpiske <piskepraveen@gmail.com>
2025-04-17 15:11:23 +0530

    disable eslint during vercel build

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["images.pexels.com","images.unsplash.com" ],
-  }
+  },
+ 
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
+
+
