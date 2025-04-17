f37c61ed797409a6e2de2f58eebf5ba8b9ce6069
praveenpiske <piskepraveen@gmail.com>
2025-04-17 15:21:22 +0530

    fix: switch to js config for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-    domains: ["images.pexels.com","images.unsplash.com" ],
+    domains: ["images.pexels.com", "images.unsplash.com"],
   },
- 
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
-
-
-
+module.exports = nextConfig;
